da63e45c6fbf3bd002b6061a2af14062eb15b8fa
Jorge Rubio <jorgerrubio@gmail.com>
2022-01-22 20:01:08 +0100

    add js-base64 add encode and decode code

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css';
 import Split from 'split-grid';
+import { encode, decode } from 'js-base64';
+
 const $ = selector => document.querySelector(selector);
 const $$ = selector => document.querySelectorAll(selector);
 
@@ -23,10 +25,25 @@ $js.addEventListener('input', update);
 $css.addEventListener('input', update);
 $html.addEventListener('input', update);
 
+function loadPage() {
+    const { pathname } = window.location;
+    const [css, html, js] = pathname.slice(1).split('%7C');
+
+    $css.value = css ? decode(css) : '';
+    $html.value = html ? decode(html) : '';
+    $js.value = js ? decode(js) : '';
+
+    update();
+}
+
 function update() {
     const css = $css.value;
     const html = $html.value;
     const js = $js.value;
+
+    const hashed = `${encode(css)}|${encode(html)}|${encode(js)}`;
+    window.history.replaceState(null, null, hashed);
+
     const htmlView = viewPage({ html, css, js });
     $('iframe').setAttribute('srcdoc', htmlView);
 }
@@ -43,3 +60,5 @@ const viewPage = ({ html, css, js }) => {
         </html>
     `;
 }
+
+loadPage();
