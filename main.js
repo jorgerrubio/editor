5b64c6ef8b921073aeea01845bc097b5c801d0d7
Jorge Rubio <jorgerrubio@gmail.com>
2022-01-23 13:56:54 +0100

    add favicons add monaco editor

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 import './style.css';
 import Split from 'split-grid';
 import { encode, decode } from 'js-base64';
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+self.MonacoEnvironment = {
+	getWorker(_, label) {
+		if (label === 'json') {
+			return new jsonWorker();
+		}
+		if (label === 'css' || label === 'scss' || label === 'less') {
+			return new cssWorker();
+		}
+		if (label === 'html' || label === 'handlebars' || label === 'razor') {
+			return new htmlWorker();
+		}
+		if (label === 'typescript' || label === 'javascript') {
+			return new tsWorker();
+		}
+		return new editorWorker();
+	}
+};
 
 const $ = selector => document.querySelector(selector);
 const $$ = selector => document.querySelectorAll(selector);
@@ -21,25 +45,49 @@ const $js = $('#js');
 const $css = $('#css');
 const $html = $('#html');
 
-$js.addEventListener('input', update);
+const theme = 'vs-dark';
+
+const cssEditor = monaco.editor.create($css, {
+    language: 'css',
+    theme,
+    value: '',
+});
+const htmlEditor = monaco.editor.create($html, {
+    language: 'html',
+    theme,
+    value: '',
+});
+const jsEditor = monaco.editor.create($js, {
+    language: 'typescript',
+    theme,
+    value: '',
+});
+
+/* $js.addEventListener('input', update);
 $css.addEventListener('input', update);
-$html.addEventListener('input', update);
+$html.addEventListener('input', update); */
+cssEditor.onDidChangeModelContent(update);
+htmlEditor.onDidChangeModelContent(update);
+jsEditor.onDidChangeModelContent(update);
 
 function loadPage() {
     const { pathname } = window.location;
     const [css, html, js] = pathname.slice(1).split('%7C');
 
-    $css.value = css ? decode(css) : '';
+    /* $css.value = css ? decode(css) : '';
     $html.value = html ? decode(html) : '';
-    $js.value = js ? decode(js) : '';
+    $js.value = js ? decode(js) : ''; */
+    cssEditor.setValue(css ? decode(css) : '');
+    htmlEditor.setValue(html ? decode(html) : '');
+    jsEditor.setValue(js ? decode(js) : '');
 
     update();
 }
 
 function update() {
-    const css = $css.value;
-    const html = $html.value;
-    const js = $js.value;
+    const css = cssEditor.getValue();
+    const html = htmlEditor.getValue();
+    const js = jsEditor.getValue();
 
     const hashed = `${encode(css)}|${encode(html)}|${encode(js)}`;
     window.history.replaceState(null, null, hashed);
